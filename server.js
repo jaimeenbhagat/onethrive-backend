8adfa74d679ebbf8f8bf491940accfae2602e8cc
jaimeenbhagat <jaimeenbhagat@outlook.com>
2025-06-30 11:32:52 +0400

    Updated original email id

--- server.js
+++ server.js
@@ -153,7 +153,7 @@ app.post('/api/contact', async (req, res) => {
 
     const mailOptions = {
       from: process.env.SENDER_EMAIL,
-      to: 'jbhagat2005@gmail.com',
+      to: 'info@onethrive.in',
       subject: emailSubject,
       html: emailBody,
       replyTo: workEmail
